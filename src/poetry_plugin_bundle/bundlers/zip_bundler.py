fa081833bff861f37d9f9d4fe96a3fdfc59d3e57
BrandonLWhite <brandonlwhite@gmail.com>
2023-02-14 11:16:30 -0600

    Use TempDirectory for virtualenv

--- src/poetry_plugin_bundle/bundlers/zip_bundler.py
+++ src/poetry_plugin_bundle/bundlers/zip_bundler.py
@@ -10,56 +10,66 @@ if TYPE_CHECKING:
 
 from .venv_bundler import VenvBundler
 
-# TODO : Should the venv directory be a temp location?  Otherwise, how to specify the zip filename.
+# TODO:
+# - Consider changing to has-a for VenvBundler instead of is-a.
+# - Flag for .so strip
+# - Flag for nested ZIP
+# - Config for strip patterns
+# - Unit tests!
+# - Improve CLI normal operational messages
+# - Improve CLI command help/info text.
+# - Debug-level output
 class ZipBundler(VenvBundler):
     name = "zip"
 
     def bundle(self, poetry: Poetry, io: IO) -> bool:
         from zipfile import ZipFile, ZIP_DEFLATED, ZIP_STORED
-        from tempfile import NamedTemporaryFile
+        from tempfile import NamedTemporaryFile, TemporaryDirectory
         from poetry.utils.env import VirtualEnv
         from poetry.core.masonry.builders.builder import Builder
         from pathlib import Path
 
-        if not super().bundle(poetry, io):
-            return False
+        zip_file_path = self._path
 
-        io.write_line('Start ZIP bundler now!')
+        with TemporaryDirectory() as temp_virtual_env:
+            temp_virtual_env_path = Path(temp_virtual_env)
+            self.set_path(temp_virtual_env_path)
+            if not super().bundle(poetry, io):
+                return False
 
+            zip_src_root_path = VirtualEnv(temp_virtual_env_path).site_packages.path
+            # print('zip_root_path', zip_root_path)
 
-        zip_root_path = VirtualEnv(self._path).site_packages.path
-        print('zip_root_path', zip_root_path)
+            project_files = set()
+            for file in Builder(poetry).find_files_to_add():
+                venv_sub_path = file.path.relative_to(poetry.file.path.parent)
+                venv_file_path = zip_src_root_path / venv_sub_path
+                project_files.add(venv_file_path)
 
-        project_files = set()
-        for file in Builder(poetry).find_files_to_add():
-            venv_sub_path = file.path.relative_to(poetry.file.path.parent)
-            venv_file_path = zip_root_path / venv_sub_path
-            project_files.add(venv_file_path)
+            # print('Include files....')
+            # for file in project_files:
+            #     print(str(file))
 
-        print('Include files....')
-        for file in project_files:
-            print(str(file))
+            # For now just make the zip file be the same name as the venv path, but with .zip extension
+            # zip_file_path = self._path.with_suffix('.zip')
 
-        # For now just make the zip file be the same name as the venv path, but with .zip extension
-        zip_file_path = self._path.with_suffix('.zip')
+            self._strip_shared_libs(zip_src_root_path)
+            file_paths = self._strip_unnecessary_files(zip_src_root_path)
 
-        self._strip_shared_libs(zip_root_path)
-        file_paths = self._strip_unnecessary_files(zip_root_path)
+            with NamedTemporaryFile() as temp_requirements_zip:
+                with ZipFile(temp_requirements_zip, mode='w', compression=ZIP_DEFLATED) as requirements_zip:
+                    for file in file_paths:
+                        if file not in project_files:
+                            requirements_zip.write(str(file), arcname=file.relative_to(zip_src_root_path))
+                        # else:
+                        #     print(f'Not writing to zip: {file}')
 
-        with NamedTemporaryFile() as temp_requirements_zip:
-            with ZipFile(temp_requirements_zip, mode='w', compression=ZIP_DEFLATED) as requirements_zip:
-                for file in file_paths:
-                    if file not in project_files:
-                        requirements_zip.write(str(file), arcname=file.relative_to(zip_root_path))
-                    else:
-                        print(f'Not writing to zip: {file}')
-
-            with ZipFile(str(zip_file_path), mode='w', compression=ZIP_DEFLATED) as zip_file:
-                zip_file.write(temp_requirements_zip.name, arcname='.requirements.zip', compress_type=ZIP_STORED)
-                unzip_stub_path = Path(__file__).parent / 'unzip_requirements.py'
-                zip_file.write(str(unzip_stub_path), arcname=unzip_stub_path.name)
-                for file in project_files:
-                    zip_file.write(str(file), arcname=file.relative_to(zip_root_path))
+                with ZipFile(str(zip_file_path), mode='w', compression=ZIP_DEFLATED) as zip_file:
+                    zip_file.write(temp_requirements_zip.name, arcname='.requirements.zip', compress_type=ZIP_STORED)
+                    unzip_stub_path = Path(__file__).parent / 'unzip_requirements.py'
+                    zip_file.write(str(unzip_stub_path), arcname=unzip_stub_path.name)
+                    for file in project_files:
+                        zip_file.write(str(file), arcname=file.relative_to(zip_src_root_path))
 
         return True
 
